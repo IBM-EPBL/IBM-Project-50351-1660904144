66dc9333ec175ab9b906ca05ded4df172ce27fd7
Priyanka <priyankap1439@gmail.com>
2022-11-06 21:01:30 +0530

    Commit

--- Project Development Phase/Sprint 2/app.py
+++ Project Development Phase/Sprint 2/app.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=2f3279a5-73d1-4859-88f0-a6c3e6b4b907.c3n41cmd0nqnrk39u98g.databases.appdomain.cloud;PORT=30756;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=jzw78279;PWD=TjUUh5loMveZUDtJ",'','')
+conn = ibm_db.connect(os.getenv('DB_KEY'),'','')
 
 app = Flask(__name__)
 
